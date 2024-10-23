f312ca397d93b38e62b7672069118b932180c47b
Giamm <vitorgiammella+git@gmail.com>
2024-10-23 09:41:44 -0300

    unit test

---
+++ src/CoreGoDelivery.ApplicationTests/Services/Internal/Rental/Commands/Create/Common/CalculateDatesByPlanTests.cs
@@ -0,0 +1,25 @@
+﻿using CoreGoDelivery.Domain.Entities.GoDelivery.RentalPlan;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Xunit.Assert;
+
+namespace CoreGoDelivery.Application.Services.Internal.Rental.Commands.Create.Common.Tests
+{
+    [TestClass()]
+    public class CalculateDatesByPlanTests
+    {
+        [Fact]
+        public void Calculate_ShouldReturnRentalEntity_WithCorrectDates()
+        {
+            var service = new CalculateDatesByPlan();
+
+            var plan = new RentalPlanEntity { DaysQuantity = 5 };
+
+            var result = service.Calculate(plan);
+
+            Assert.NotNull(result);
+            Assert.Equal(DateTime.UtcNow.Date, result.StartDate.Date);
+            Assert.Equal(DateTime.UtcNow.AddDays(plan.DaysQuantity).Date, result.EndDate.Date);
+            Assert.Equal(DateTime.UtcNow.AddDays(plan.DaysQuantity).Date, result.EstimatedReturnDate.Date);
+        }
+    }
+}
