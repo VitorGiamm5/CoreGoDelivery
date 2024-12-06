33675a13970120a687674ae291a8fb85698ef786
VitorGiamm5 <151173779+VitorGiamm5@users.noreply.github.com>
2024-12-06 09:03:21 -0300

    Feature/connection-minio (#6)
    * connection minio
    
    * remove test connection
    
    * space
    
    * minio injection

--- src/CoreGoDelivery.Api/Program.cs
+++ src/CoreGoDelivery.Api/Program.cs
@@ -74,6 +74,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers().WithMetadata(new RouteAttribute("api/[controller]"));
+
 app.UseAuthorization();
 
 app.UseCors("AllowAll");
